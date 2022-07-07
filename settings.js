64f37e73eeb72f02d91cba461c6fde4cf4f80510
Mikhaiel_Offical <mikhaieloffical@gmail.com>
2022-07-07 13:28:01 +0530

    Initial commit

---
+++ settings.js
@@ -0,0 +1,70 @@
+//═══════════════════════════════════════════════════════//
+// mikhaiel offical
+//════════════════════════════//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': '2D8C144E4C', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+//customize settings\\
+global.owner = ['919544846609','919746319042']
+global.premium = ['919544846609','919746319042']
+global.ownernomer = '919544846609','919746319042'
+global.ownername = 'Mikhaiel'
+global.botname = 'Jimbru v4.1'
+global.footer = '© JIMBRU OFFICAL'
+global.ig = 'https://github.com/Mikhaiel'
+global.region = 'India, southwestern, kerala'
+global.sc = 'https://github.com/Mikhaiel/Jimbru v4.1'
+global.myweb = 'https://instagram.com/the_real_mikhaiel'
+global.packname = 'Made By'
+global.author = 'Jimbru v4.1'
+global.sessionName = 'session'
+global.prefa = ['','!','.']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'In Process...',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 999999,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./Media/Jimbru.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
